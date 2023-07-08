c117995608e7d6f89f909408cabef268af20e77d
Pter <gtlpd21@gmail.com>
2023-07-09 02:10:17 +0800

    added token verification to prevent exploitation

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ import io
 import base64
 from PIL import Image
 from transformers import pipeline
-
+import os
 app = FastAPI()
 
 class ImageType(BaseModel):
     data: List[str] = []
+    token: str
 
 # Load image captioning model on startup
 @app.on_event('startup')
@@ -17,24 +18,35 @@ async def load_model():
     global captioner
     captioner = pipeline("image-to-text", model="Salesforce/blip2-opt-2.7b-coco")
 
+# Generate API Token Randomly on startup and print that to console
+@app.on_event('startup')
+async def generate_token():
+    global token
+    token = base64.b64encode(os.urandom(32)).decode('utf-8')
+    print(f'API Token: {token}')
+
+
 @app.post('/predict')
 async def predict(images: ImageType):
     try:
         captions = []
-        for image_data in images.data:
-            # convert base64 string back into bytes
-            image_bytes = base64.b64decode(image_data)
+        if images.token != token:
+            raise HTTPException(status_code=401, detail="Unauthorized")
+        else:
+            for image_data in images.data:
+                # convert base64 string back into bytes
+                image_bytes = base64.b64decode(image_data)
 
-            # open the image
-            image = Image.open(io.BytesIO(image_bytes))
+                # open the image
+                image = Image.open(io.BytesIO(image_bytes))
 
-            # use the model to predict
-            result = captioner(image)[0]
+                # use the model to predict
+                result = captioner(image)[0]
 
-            # append the caption to the list of captions
-            captions.append(result['generated_text'])
+                # append the caption to the list of captions
+                captions.append(result['generated_text'])
 
-        return {'captions': captions}
+            return {'captions': captions}
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
